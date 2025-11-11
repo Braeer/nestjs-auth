354c1f9ac1919485e605ae647576a82344c63948
Braeer <developer.timurbagaev@gmail.com>
2025-11-11 17:53:56 +0300

    ADD: prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    Logger.log('Connecting to the database...');
+    await this.$connect();
+    Logger.log('Connected to the database');
+  }
+
+  public async onModuleDestroy() {
+    Logger.log('Disconnecting from the database...');
+    await this.$disconnect();
+    Logger.log('Database disconnected.');
+  }
+}
